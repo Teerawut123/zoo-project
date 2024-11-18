f1eae3c3c19c9c5ac548821b0965df1505571ec1
Teerawut123 <shadowk464@gmail.com>
2024-11-18 12:06:23 +0000

    Added the test cases

--- test_zoo.py
+++ test_zoo.py
@@ -10,5 +10,29 @@ class TestZoo(unittest.TestCase):
        
     # Add your additional test cases here.
 
+    def test_negative_age(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), "Error (Invalid Age)")
+
+    def test_age_zero(self):
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)
+
+    def test_age_twelve(self):
+        self.assertEqual(self.zoo.get_ticket_price(12), 50)
+
+    def test_age_thirteen(self):
+        self.assertEqual(self.zoo.get_ticket_price(13), 100)
+
+    def test_age_twenty(self):
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
+
+    def test_age_twentyone(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+
+    def test_age_sixty(self):
+        self.assertEqual(self.zoo.get_ticket_price(60), 150)
+
+    def test_age_sixtyone(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
+
 if __name__ == '__main__':
     unittest.main()
